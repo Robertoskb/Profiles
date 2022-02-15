8dd2056e4f734d283f171d8340276d34a08f2f40
Robertoskb <97955337+Robertoskb@users.noreply.github.com>
2022-02-15 13:46:35 -0300

    .

--- Profiles.py
+++ Profiles.py
@@ -3,12 +3,11 @@ import csv
 class File:
     #faz tratamento do arquivo csv onde ficará salvo os perfis
 
-    def __init__(self, file_csv = 'file.csv', items = dict()):
+    def __init__(self, file_csv = 'file.csv'):
         self._CreateFile(file_csv)
 
         self.saves = self._ReadFile(file_csv)
         self.file = file_csv
-        self.items = items
         
 
     def _CreateFile(self, filename):       
@@ -24,36 +23,35 @@ class File:
 
     def _ReadFile(self, filename):
         a = open(filename,'r')
+        b = csv.reader(a)
 
-        reader = csv.reader(a)
         saves = list()
     
-        for line in reader: saves.append(line)
+        for line in b: saves.append(line)
 
         a.close()
 
         return saves
 
 
-    def _SaveProfile(self):
-        self.list = [self.name, self.items]
+    def _SaveProfile(self, name, items):
+        self.list = [name, items]
 
         a = open(self.file, 'a', newline='')
+        b = csv.writer(a)
 
-        recorder = csv.writer(a)
-        recorder.writerow(self.list)
+        b.writerow(self.list)
 
         a.close()
 
-        self.__init__(self.file, self.items)
+        self.__init__(self.file)
 
 
     def _SaveProgress(self):
         a = open(self.file, 'w', newline='')
+        b = csv.writer(a)
 
-        recorder = csv.writer(a)
-
-        for i in self.saves: recorder.writerow(i)
+        for i in self.saves: b.writerow(i)
 
         a.close()
 
@@ -61,18 +59,16 @@ class File:
 class NewProfile(File):
     #cria, carrega e deleta perfis salvos
 
-    def __init__(self, file_csv = 'file.csv', items = dict()):
-        File.__init__(self, file_csv, items)
+    def __init__(self, file_csv = 'file.csv'):
+        File.__init__(self, file_csv)
     
 
-    def CreateProfile(self, name):
+    def CreateProfile(self, name: str, items: dict):
         name = name.strip()
 
         if name == '': name = 'Profile'
         
-        self.name = name
-        
-        self._SaveProfile()
+        self._SaveProfile(name, items)
 
 
     def LoadProfile(self, id):        
@@ -82,13 +78,14 @@ class NewProfile(File):
         return name, items
     
            
-    def DeleteProfile(self, id, msg = 'Profile not found'):
+    def DeleteProfile(self, id: int, msg = 'Profile not found'):
         try:
             self.saves.pop(id)
             self._SaveProgress()
                
         except:
             print(msg)
+
 
     def DelAllProfiles(self):
         self.saves.clear()
@@ -98,12 +95,12 @@ class NewProfile(File):
 class LayoutProfile(NewProfile):
     #Retorna uma string que com os perfis salvos
 
-    def __init__(self, file_csv = 'file.csv', items = dict()):
-        NewProfile.__init__(self, file_csv, items)
+    def __init__(self, file_csv = 'file.csv'):
+        NewProfile.__init__(self, file_csv)
 
 
     def ListProfiles(self, msg = 'No profiles'):
-        self.__init__(self.file, self.items)
+        self.__init__(self.file)
 
         text = ''
 
@@ -116,12 +113,12 @@ class LayoutProfile(NewProfile):
 
 
     def ListProfilesCompletely(self, msg = 'No profiles'):
-        self.__init__(self.file, self.items)
+        self.__init__(self.file)
 
         text = ''
 
         for c, i in enumerate(self.saves): 
-            text += f'{c+1} - {self.saves[c][0]} id: {c}\n{self.saves[c][1]} \n'
+            text += f'{c+1} - {self.saves[c][0]} id: {c}\n{self.saves[c][1]}\n'
 
         if text == '': return msg
 
@@ -129,26 +126,28 @@ class LayoutProfile(NewProfile):
 
 
 class ModifyProfile(LayoutProfile):
-    '''Edita nomes e items de um perfil específico'''
+    #Edita nomes e items de um perfil específico
 
-    def __init__(self, file_csv = 'file.csv', items = dict()):
-        LayoutProfile.__init__(self, file_csv, items)
+    def __init__(self, file_csv = 'file.csv'):
+        LayoutProfile.__init__(self, file_csv)
 
       
-    def EditName(self, id, name, msg = 'Profile not found'):
+    def EditName(self, id: int, name: str, msg = 'Profile not found'):
         try:
             self.saves[id][0] = name
+
             self._SaveProgress()
         
         except:
             print(msg)
+            
 
-    def UpdateItems(self, id, items, msg = 'Profile not found'):
+    def UpdateItems(self, id: int, items: dict, msg = 'Profile not found'):
         try:
             self.saves[id][1] = eval(self.saves[id][1])
             self.saves[id][1] = items
+
             self._SaveProgress()
         
         except:
             print(msg)
-    
--- teste.py
+++ teste.py
@@ -2,6 +2,5 @@ import Profiles
 
 a = Profiles.ModifyProfile('exemplo01.csv')
 
-a.CreateProfile('Roberto')
-a.UpdateItems(0,{'Cubos':['Skewb', '3x3x3', 'Megaminx']})
-
+a.CreateProfile('Roberto',{})
+a.UpdateItems(0, {'cubos': ['Skewb', '3x3x3', 'Megaminx']})
